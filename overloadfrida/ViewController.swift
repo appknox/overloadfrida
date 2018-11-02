73528bb93c653e004b3fd6c9edaa6624101702cd
Sharat M R <cosmosgenius@gmail.com>
2018-11-02 17:58:56 +0530

    Add overload button

--- overloadfrida/ViewController.swift
+++ overloadfrida/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var iterationInput: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func overloadNSLog(sender: UIButton) {
+        let num:Int = Int(iterationInput.text!) ?? 0
+        let alertController = UIAlertController(title: "Hello", message: "Hello \(num + 1)",
+                                                preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+        var i = 0
+        while i < num {
+            i += 1
+            NSLog("This should be working \(num)" )
+        }
+    }
 }
-
