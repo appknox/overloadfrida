077f4e75e5e4aed2dfcdf9d79b480923c6e965bf
Sharat M R <cosmosgenius@gmail.com>
2019-12-18 14:01:56 +0530

    Add appcenter integration

---
+++ appcenter-post-build.sh
@@ -0,0 +1,10 @@
+if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
+    if [ "$APPCENTER_BRANCH" == "master" ];
+     then
+        curl -L https://github.com/appknox/appknox-go/releases/download/1.1.0/appknox-`uname -s`-x86_64 > appknox && chmod +x appknox
+        ./appknox upload $APPCENTER_OUTPUT_DIRECTORY/overloadfrida.ipa
+        rm appknox
+    else
+        echo "Current branch is $APPCENTER_BRANCH"
+    fi
+fi
